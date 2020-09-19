94a0a9460bd77fb9a24d14f773bd7edd4f69f5a1
Weyler N M Lopes <38050619+weylerl@users.noreply.github.com>
2020-09-18 22:59:11 -0300

    Primeira atualização

---
+++ Teste_Serializacao/src/Teste01/DeserializacaoTeste.java
@@ -0,0 +1,28 @@
+package Teste01;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+import Teste01.Cliente;
+
+public class DeserializacaoTeste {
+    public static void main(String[] args)
+    {
+		ArrayList<Cliente> clientes = null;
+
+		try
+        {
+            FileInputStream fis = new FileInputStream("clis.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            clientes = (ArrayList<Cliente>)ois.readObject();
+            ois.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace(); }
+        for (Cliente cli: clientes)
+            System.out.println(cli);
+        }
+
+}
